ddb3a652ad85cb50d19852d5b65de665c9bce088
VVNoodle <brickkace@gmail.com>
2017-11-02 14:48:49 -0700

    Search on type, still under construction

--- js/index.js
+++ js/index.js
@@ -1,59 +1,76 @@
-$(document).ready(function(){
-  
- $("#button").click(function(){  
-	 
-	 $('body').attr('style', "background-size: default");
-	
-	 
-   //Get user input
-   var term = $("#userSearch").val();
+$(document).ready(function() {
+  $("#button").click(function() {
+    $("h2").css("margin", "40px 0px 40px 0px");
+    $("body").attr("style", "background-size: default");
+
+    //Get user input
+    var term = $("#userSearch").val();
 
     //Open Search action for wiki API
-    var link = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + term +"&format=json&callback=?";
-	 	 
-   
+    var link =
+      "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
+      term +
+      "&format=json&callback=?";
+
     //API call
     $.ajax({
-      type:"GET",
-      url:link,
-      async:false,
-      dataType:"json",
-      success: function(array){
-				if(term !== ""){
-        $("#results").html("Results for: " + term);
-				
-				var desCount = 0;
-			  var greyscale = "greyscale";
-			  // var animation = "animated fadeInDown";
-				var descID = "description";
-				var newTab = "_blank";
-				
-				//reset search if user searches again
-				$("#choices").html("");
-				
-				//Search results. Title and description
-				 array[1].forEach(function(val){
-					 if(term.toUpperCase() !==  val.toUpperCase()){ //Prevent "term refers to", it's redundant
-          $("#choices").append(
-						'<a href='+array[3][desCount]+' target =' +newTab+ '><p class='+greyscale+'><b>' + val + '</b></p></a>'					 
-						+'<p id='+descID+'>' +array[2][desCount++] + '</p><br/>'); //end append Title
-				 }
-					 else desCount++;
-        });//end foreach
-					
-					$("#userSearch").val('');
-				}
+      type: "GET",
+      url: link,
+      async: false,
+      dataType: "json",
+      success: function(array) {
+        if (term !== "") {
+          $("#results").html("Results for: " + term);
+
+          var desCount = 0;
+          var greyscale = "greyscale";
+          // var animation = "animated fadeInDown";
+          var descID = "description";
+          var newTab = "_blank";
+
+          //reset search if user searches again
+          $("#choices").html("");
+
+          //Search results. Title and description
+          array[1].forEach(function(val) {
+            if (term.toUpperCase() !== val.toUpperCase()) {
+              //Prevent "term refers to", it's redundant
+              $("#choices").append(
+                "<a href=" +
+                  array[3][desCount] +
+                  " target =" +
+                  newTab +
+                  "><p class=" +
+                  greyscale +
+                  "><b>" +
+                  val +
+                  "</b></p></a>" +
+                  "<p id=" +
+                  descID +
+                  ">" +
+                  array[2][desCount++] +
+                  "</p><br/>"
+              ); //end append Title
+            } else desCount++;
+          }); //end foreach
+
+          // $("#userSearch").val("");
+        }
       },
-      error: function(error){
+      error: function(error) {
         alert("ERROR");
       }
-    }); //end ajax   
-  });  //end button click
-	
-	//Allow user to search using Enter/Return (ascii value is 13)
-	$("#userSearch").keypress(function(key){
-		 if(key.which === 13){
-			 $("#button").click();
-		 }
-	 });
+    }); //end ajax
+  }); //end button click
+
+  //Allow user to search using Enter/Return (ascii value is 13)
+  $("#userSearch").keypress(function(key) {
+    if (
+      (key.which >= 65 && key.which <= 90) ||
+      (key.which >= 97 && key.which <= 122)
+    ) {
+      console.log("TEST");
+      $("#button").click();
+    }
+  });
 });
